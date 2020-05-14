f952885b4a4688255fdbdfe2e9e81405962295d9
Noblepal <zeronillzero@gmail.com>
2020-05-14 17:26:32 +0300

    removed test apk file; re-enabling maintenance mode

--- functions.php
+++ functions.php
@@ -1,13 +1,12 @@
 <?php
 
+$maintenance = true;
 
-$maintenance = false;
-
-$download_url = "app/covid-19.apk";
-
-if($maintenance){
+if ($maintenance) {
     echo "Coming soon...";
     die;
 }
+
+$download_url = "app/covid-19.apk";
 
 $con = mysqli_connect('localhost', 'vootbxke_covid', '*7^Fi5UY3De^FEC4iV1O45cy0vlxP5kF', 'vootbxke_covid_19');
